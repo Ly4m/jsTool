3cb21980c7c3e9f23c18c017742a678bcb6bbd46
Ly4m <will.leemans@gmail.com>
2015-11-15 14:51:24 +0100

    fix if ok
    Signed-off-by: Ly4m <will.leemans@gmail.com>

--- jsTool.js
+++ jsTool.js
@@ -3,6 +3,7 @@ var esprima = require('esprima');
 var estraverse = require('estraverse');
 
 var scopeChaine = [];
+var typeChaine = [];
 
 var filename = process.argv[2];
 console.log("analyse :", filename);
@@ -19,20 +20,66 @@ estraverse.traverse(ast, {
 
 		if(createsNewScope(node)){
 			scopeChaine.push([]);
+			typeChaine.push([]);
 		}
 
 		if(node.type === 'VariableDeclarator'){
 			var currentScope = scopeChaine[scopeChaine.length - 1];
+			var currentScopeType = typeChaine[typeChaine.length -1];
 			currentScope.push(node.id.name);
+			currentScopeType.push(typeof node.init.value);
 		}
 	},
 
 	leave: function(node){
 
 		leave(node);
+		ifFix(node);
 
 	}
 });
+
+
+function ifFix(node){
+
+
+	if(node.type === 'IfStatement' && node.test.operator === "=="){
+		
+		var leftType = node.test.left.value;
+		
+		if(leftType != undefined){
+			leftType = typeof leftType;
+		} else {
+			leftType = typeLookUp(node.test.left.name);
+		}
+
+		var rightType = node.test.right.value;
+	
+		if(rightType != undefined){
+			rightType = typeof rightType;
+		} else {
+			rightType = typeLookUp(node.test.right.name);
+		}
+
+		if(leftType == rightType){
+			
+			console.log("il faut changer", leftType, "==", rightType);
+		} else {
+			console.log("il ne faut pas changer", leftType, "==", rightType);
+		}
+	}
+}
+
+function typeLookUp(name){
+	for (i = scopeChaine.length - 1;  i >= 0 ; i-- ){
+		
+		var indexVar = scopeChaine[i].indexOf(name);
+		
+		if(indexVar != -1){
+			return typeChaine[i][indexVar];
+		}
+	}
+}
 
 function createsNewScope(node){
 	return node.type === 'FunctionDeclaration' || 
@@ -43,6 +90,30 @@ function createsNewScope(node){
 function leave(node){
 	if(createsNewScope(node)){
 		var currentScope = scopeChaine.pop();
-		console.log(currentScope);
+		var currentScopeType = typeChaine.pop();
+		//printScope(currentScope, node);
 	}
+}
+
+function printScope(scope, node){
+	var varsDisplay = scope.join(', ');
+	if(node.type === 'Program'){
+		console.log('Variable définies dans le scope global :', varsDisplay);
+	} else {
+		if(node.id && node.id.name){
+			console.log('Variables declared in the function ' + node.id.name + '();', varsDisplay);
+		} else {
+			console.log('Variables declared in anonymous function:', varsDisplay);
+		}
+	}
+}
+
+function isVarDefined(varname, scopeChaine){
+	for(var i = 0; i < scopeChaine.length; i++){
+		var scope = scopeChaine[i];
+		if(scope.indexOf(varname) !== -1){
+			return true;
+		}
+	}
+	return false;
 }
--- test.js
+++ test.js
@@ -5,7 +5,16 @@ var y = 3;
 function maFonction(){
 	var z = 1;
 
-	if(z == 1){
+	(function g(){
+		var d = "yo";
+
+		if(d == z){
+			console.log("test KO");
+		}
+
+	}());
+
+	if(z == y){
 		console.log("test OK");
 	}
 }
